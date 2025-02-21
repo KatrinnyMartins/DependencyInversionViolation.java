78e48cab1b8157980d55dad79ebb0adfd9fd5689
KatrinnyMartins <katrinny.l.martins@gmail.com>
2025-02-20 22:15:57 -0300

    Create LiskovSubstitutionViolation.java

---
+++ LiskovSubstitutionViolation.java
@@ -0,0 +1,36 @@
+public class LiskovSubstitutionCorreta {
+
+    public static void main(String[] args) {
+        FlyingBird sparrow = new Sparrow();
+        sparrow.fly();
+
+        NonFlyingBird ostrich = new Ostrich();
+        ostrich.walk();
+    }
+}
+
+interface FlyingBird {
+    void fly();
+}
+
+interface NonFlyingBird {
+    void walk();
+}
+
+class Sparrow implements FlyingBird {
+    public void fly() {
+        System.out.println("Sparrow is flying");
+    }
+}
+
+class Ostrich implements NonFlyingBird {
+    public void walk() {
+        System.out.println("Ostrich is walking");
+    }
+}
+
+
+
+
+
+
